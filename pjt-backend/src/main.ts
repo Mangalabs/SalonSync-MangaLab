4a04172be94c9d189445b9f73dfcccef7dbf5331
Diogo-Felipe <dlcfelipp@gmail.com>
2025-08-11 04:40:59 -0300

    .env to cors

--- pjt-backend/src/main.ts
+++ pjt-backend/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'https://salondash.mangalab.io' });
+  app.enableCors({ origin: process.env.ALLOWED_ORIGINS?.split(',') });
 
   app.setGlobalPrefix('api');
 
