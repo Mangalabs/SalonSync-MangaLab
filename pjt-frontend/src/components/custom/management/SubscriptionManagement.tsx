fb0df60f986e52e8e6f01f2fd7828520ec62d1ed
Thicianny <thicianny2@gmail.com>
2025-09-21 22:10:56 -0300

    feat: config page

--- pjt-frontend/src/components/custom/management/SubscriptionManagement.tsx
+++ pjt-frontend/src/components/custom/management/SubscriptionManagement.tsx
@@ -2,7 +2,7 @@ import CheckoutForm from '@/components/pages/CheckoutForm'
 
 export function SubscriptionManagement() {
   return (
-    <div className="w-full max-w-md mx-auto">
+    <div>
       <CheckoutForm />
     </div>
   )
